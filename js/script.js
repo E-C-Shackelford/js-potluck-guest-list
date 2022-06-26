32a970491863175b1352486fcfbf0d46f555b471
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-25 18:33:12 -0700

    Select assigned items, build an array, and select all list elements inside the list and loop through the array so that a list of each guest and their assigned dish displays

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
 // invite button
 const addGuestButton = document.querySelector(".invite");
+
 // invite button label
 const guestInputLabel = document.querySelector(".add-guest label");
+
 // text input box
 const guestInput = document.querySelector(".add-guest input");
+
 // unordered list
 const guestList = document.querySelector(".guest-list");
+
 // span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
+
 // alert when guest list is full
 const guestFull = document.querySelector(".alert");
+
+// select element with assign class, which only appears when the guest list is full
+const assignButton = document.querySelector(".assign");
+
+// select element with assigned-items class, which targets the list that will populate with the guest’s name and assigned dish
+const assignedItems = document.querySelector(".assigned-items");
 
 // ***** Add an Event Listener & Create an Element *****
 
@@ -56,5 +67,46 @@ const updateGuestCount = function () {
     guestInput.classList.add("hide");
     guestInputLabel.classList.add("hide");
     guestFull.classList.remove("hide");
+  }
+};
+
+// ***** build an array, and then select elements and loop through the array *****
+
+const assignItems = function () {
+  const potluckItems = [
+    "mixed fruit plater",
+    "mixed veggie plater",
+    "lemon-ginger hummus and pumpkin-chia seed crackers",
+    "gazpacho soup",
+    "guacamole and blue corn chips",
+    "salsa",
+    "veggie nori rolls",
+    "roasted tomato and carmelized sweet onion tart",
+    "garbanzo based pasta salad with summer veggies and lemon-tahini dressing",
+    "canneli bean, bell pepper, and lemon-basil quinoa salad",
+    "cilantro-pepita pesto with black forbidden rice and veggie salad",
+    "roasted veggie kabobs with sundried tomato dipping sauce",
+  ];
+
+  // select all list elements inside the unordered list with a class of “guest-list”
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  // loop over the allGuests array using guest as the variable
+  for (let guest of allGuests) {
+    // generate a number between 0 and the length of potluckItems to select a random element from the array
+    // use Math.floor() function since it returns the largest integer less than or equal to a given number
+    // use Math.random() function since it returns a floating-point, pseudo-random number in the range 0 to less than 1 (inclusive of 0, but not 1)
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+
+    // in the value position, add the item from the potluckItems array at the index position of randomPotluckIndex
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    // create a new "li" element
+    let listItem = document.createElement("li");
+
+    // set the innerText of listItem to a string with the guest name and item they're bringing
+    // use guest.innerText to access the name inside the li element because if you instead just use guest without innerText, you’d grab the actual list element instead of the text
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
   }
 };
